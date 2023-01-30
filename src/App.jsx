b6aa6c1a8ba80bc095c17f1667ff533445f6afce
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-01-30 13:08:45 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,15 @@ import Header from "./components/header";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
-import Carousel from "./components/carousel";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
+      <About />
       <Contact />
-    </BrowserRouter>
+    </>
   );
 }
 
